a3f5659423c3f69609e0801879593ddd203bb48c
max-nb <maxi.biscaia54@gmail.com>
2022-06-28 23:11:06 -0300

    feat: criada estrutura de pastas, modulos e componentes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout.module';
+import { RoutesModule } from './routes/routes.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    RoutesModule,
+    HttpClientModule,
+    SharedModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+        ],
+        canActivate:[],
+    },
+
+    
+    {path: '**', redirectTo: 'home'}
+]
